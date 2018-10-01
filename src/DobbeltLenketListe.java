da0c6e360a808e35fc88bbe643b73b3f70cafef1
Bjørnar Hoff <bjornar_hoff@hotmail.no>
2018-10-01 14:18:49 +0200

    Oppgave 1 er gjort (?)

--- src/DobbeltLenketListe.java
+++ src/DobbeltLenketListe.java
@@ -1,94 +1,211 @@
+import java.util.Comparator;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class DobbeltLenketListe<T> implements Liste<T>
 {
+    private static final class Node<T>   // en indre nodeklasse
+    {
+        // instansvariabler
+        private T verdi;
+        private Node<T> forrige, neste;
+
+        private Node(T verdi, Node<T> forrige, Node<T> neste)  // konstruktør
+        {
+            this.verdi = verdi;
+            this.forrige = forrige;
+            this.neste = neste;
+        }
+
+        protected Node(T verdi)  // konstruktør
+        {
+            this(verdi, null, null);
+        }
+
+    } // Node
+
+    // instansvariabler
+    private Node<T> hode;          // peker til den første i listen
+    private Node<T> hale;          // peker til den siste i listen
+    private int antall;            // antall noder i listen
+    private int endringer;   // antall endringer i listen
+
+    // hjelpemetode
+    private Node<T> finnNode(int indeks)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
+    }
+
+    // konstruktør
+    public DobbeltLenketListe()
+    {
+        hode = hale = null;
+        antall = 0;
+        endringer = 0;
+    }
+
+    // konstruktør
+    public DobbeltLenketListe(T[] a)
+    {
+        this();
+        Objects.requireNonNull(a,"Tabellen a er null!");
+
+        int i = 0;
+        while (i < a.length && a[i] == null) i++;
+
+        if (i < a.length) {
+            Node<T> node = hode = hale = new Node<>(a[i], null, null);
+            antall++;
+            i++;
 
 
-    @Override
-    public boolean leggInn(T verdi) {
-        return false;
+            for (; i < a.length; i++) {
+                if (a[i] != null)
+                {
+                    node = node.neste = new Node<>(a[i],node,null);
+                    antall++;
+                }
+            } hale = node;
+        }
+    }
+
+    // subliste
+    public Liste<T> subliste(int fra, int til)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public void leggInn(int indeks, T verdi) {
-
+    public int antall()
+    {
+        return antall;
     }
 
     @Override
-    public boolean inneholder(T verdi) {
-        return false;
+    public boolean tom()
+    {
+        return antall == 0;
     }
 
     @Override
-    public T hent(int indeks) {
-        return null;
+    public boolean leggInn(T verdi)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public int indeksTil(T verdi) {
-        return 0;
+    public void leggInn(int indeks, T verdi)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public T oppdater(int indeks, T verdi) {
-        return null;
+    public boolean inneholder(T verdi)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public boolean fjern(T verdi) {
-        return false;
+    public T hent(int indeks)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public T fjern(int indeks) {
-        return null;
+    public int indeksTil(T verdi)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public int antall() {
-        return 0;
+    public T oppdater(int indeks, T nyverdi)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public boolean tom() {
-        return false;
+    public boolean fjern(T verdi)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public void nullstill() {
-
+    public T fjern(int indeks)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public Iterator<T> iterator() {
-        return null;
+    public void nullstill()
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public void forEach(Consumer<? super T> action) {
+    public String toString()
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
+    }
 
+    public String omvendtString()
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
+    }
+
+    public static <T> void sorter(Liste<T> liste, Comparator<? super T> c)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
     @Override
-    public Spliterator<T> spliterator() {
-        return null;
+    public Iterator<T> iterator()
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
-    @Override
-    public boolean fjernHvis(Predicate<? super T> p) {
-        return false;
+    public Iterator<T> iterator(int indeks)
+    {
+        throw new UnsupportedOperationException("Ikke laget ennå!");
     }
 
-    @Override
-    public String melding(int indeks) {
-        return null;
-    }
+    private class DobbeltLenketListeIterator implements Iterator<T>
+    {
+        private Node<T> denne;
+        private boolean fjernOK;
+        private int iteratorendringer;
 
-    @Override
-    public void indeksKontroll(int indeks, boolean leggInn) {
+        private DobbeltLenketListeIterator()
+        {
+            denne = hode;     // denne starter på den første i listen
+            fjernOK = false;  // blir sann når next() kalles
+            iteratorendringer = endringer;  // teller endringer
+        }
 
-    }
-}
+        private DobbeltLenketListeIterator(int indeks)
+        {
+            throw new UnsupportedOperationException("Ikke laget ennå!");
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            return denne != null;  // denne koden skal ikke endres!
+        }
+
+        @Override
+        public T next()
+        {
+            throw new UnsupportedOperationException("Ikke laget ennå!");
+        }
+
+        @Override
+        public void remove()
+        {
+            throw new UnsupportedOperationException("Ikke laget ennå!");
+        }
+
+    } // DobbeltLenketListeIterator
+
+} // DobbeltLenketListe
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("OBLIG 2 er opprettet");
 
-        Liste<String> liste = new DobbeltLenketListe<>();
+        /* Liste<String> liste = new DobbeltLenketListe<>();
+        System.out.println(liste.antall() + " "+ liste.tom()); */
+
+        String[] s = {"Ole", null, "Per", "Kari", null};
+        Liste<String> liste = new DobbeltLenketListe<>(s);
+        System.out.println(liste.antall() + " "+ liste.tom());
     }
 }
